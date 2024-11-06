fe3f0b57cf5ca541d7c99a05098c640406f7b9d7
Kᴏɴsᴛᴀɴᴛɪɴ Sʜɪʟᴏᴠ <shilovk@gmail.com>
2024-11-06 10:49:54 +0300

    feat(rails): update for Rails 7.2.2 feat(upgrade): use Ruby 3.3.6, Rails 7.2.2, fix code and refactor

--- lib/devise_jwt_auth/blacklist.rb
+++ lib/devise_jwt_auth/blacklist.rb
@@ -1,4 +1,10 @@
 # frozen_string_literal: true
 
-# don't serialize tokens
-Devise::Models::Authenticatable::BLACKLIST_FOR_SERIALIZATION << :tokens
+# Exclude :tokens from serialization based on Rails version
+blacklist_attribute = if Gem::Version.new(Rails.version) < Gem::Version.new("7.2.2")
+                        Devise::Models::Authenticatable::BLACKLIST_FOR_SERIALIZATION
+                      else
+                        Devise::Models::Authenticatable::UNSAFE_ATTRIBUTES_FOR_SERIALIZATION
+                      end
+
+blacklist_attribute << :tokens
